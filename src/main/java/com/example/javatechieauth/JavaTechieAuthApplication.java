ba29b1f979895caf9206038cdf5caf20b92a8ea2
Sultanmurad <sultanmuradsafarov@gmail.com>
2023-06-07 04:03:13 +0400

    set jwt authentication

--- src/main/java/com/example/javatechieauth/JavaTechieAuthApplication.java
+++ src/main/java/com/example/javatechieauth/JavaTechieAuthApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaTechieAuthApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(JavaTechieAuthApplication.class, args);
     }
--- src/main/java/com/example/javatechieauth/controller/ProductController.java
+++ src/main/java/com/example/javatechieauth/controller/ProductController.java
@@ -1,10 +1,16 @@
 package com.example.javatechieauth.controller;
 
+import com.example.javatechieauth.config.auth.JwtService;
+import com.example.javatechieauth.dto.LoginDto;
 import com.example.javatechieauth.dto.Product;
 import com.example.javatechieauth.entity.UserInfo;
 import com.example.javatechieauth.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +26,8 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService service;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -41,6 +49,16 @@ public class ProductController {
     @PostMapping("/save")
     public void save(@RequestBody UserInfo user) {
         service.save(user);
+    }
+
+    @PostMapping("/authenticate")
+    public String authenticateAndGetToken(@RequestBody LoginDto loginDto) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
+        if(authentication.isAuthenticated()) {
+            return jwtService.generateToken(loginDto.getUsername());
+
+        }
+        else throw new UsernameNotFoundException("Invalid user request. User not found.");
     }
 
 }
